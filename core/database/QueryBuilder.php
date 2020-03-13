68c521f941346c62cd79093dda9bea67735e93c1
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-13 17:55:14 +0800

    add DB ops

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -11,13 +11,46 @@ class QueryBuilder
     
     public function createStatement()
     {
+
+        $statement = $this->pdo->prepare("create table posts(
+            id INT(10) AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(12) NOT NULL,
+            content VARCHAR(255) NOT NULL,
+            created_at DATETIME
+        );");
+        echo $statement->execute();
+
+        $statement = $this->pdo->prepare("create table replies(
+            id INT(10) AUTO_INCREMENT PRIMARY KEY,
+            post_id INT NOT NULL,
+            FOREIGN KEY (post_id) REFERENCES posts(id),
+            name VARCHAR(12) NOT NULL,
+            content VARCHAR(255) NOT NULL,
+            created_at DATETIME
+        );");
+        echo $statement->execute();
     }
 
     public function selectAll($table)
     {
-        $statement = $this->pdo->prepare("select * from $table");
+        $statement = $this->pdo->prepare("select * from $table ORDER BY created_at desc");
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_CLASS);
+    }
+    public function storePost($post)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO posts (name, content, created_at) VALUES ('{$post['name']}', '{$post['content']}', '{$post['created_at']}')");
+        $statement->execute();
+    }
+    public function storeReply($reply)
+    {
+        $statement = $this->pdo->prepare("INSERT INTO replies (post_id, name, content, created_at) VALUES ('{$reply['post_id']}', '{$reply['name']}', '{$reply['content']}', '{$reply['created_at']}')");
+        $statement->execute();
+    }
+    public function showReplies($post)
+    {
+        $statement = $this->pdo->prepare("select * from posts where id= $post");
+        $statement->execute();
     }
 }
 ?>
