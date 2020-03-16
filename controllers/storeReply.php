0adebb6a2c64502eb1da170c7daea818fc2474b3
Yu-hanCheng <sarahcheng1231@gmail.com>
2020-03-16 10:08:10 +0800

    storeReply

---
+++ controllers/storeReply.php
@@ -0,0 +1,19 @@
+<?php
+require 'core/bootstrap.php';
+use Carbon\Carbon;
+
+if ($_POST['type']) {
+    $db->storeReReply([
+    'reply_id'=>$_POST['reply_id'],
+    'name'=>$_COOKIE['user_name'],
+    'content'=>$_POST['content'],
+    'created_at' => Carbon::now(),
+    ]);
+} else {
+    $db->storeReply([
+        'post_id'=>$_POST['post_id'],
+        'name'=>$_COOKIE['user_name'],
+        'content'=>$_POST['content'],
+        'created_at' => Carbon::now(),
+    ]);
+}
